6d309d3fccf53638f69ea3c43e01dba016ba7b88
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-06-06 08:46:22 -0600

    Profile card wip

--- src/components/TargetDomainProfile/TargetDomainProfile.js
+++ src/components/TargetDomainProfile/TargetDomainProfile.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './TargetDomainProfile.scss'
 
 
-export default TargetDomainProfile = ({ name, knownIpAddresses }) => (
-    <div className="target-domain-profile">
-        <header className="target-domain-profile-name">
-            Domain Name
-        </header>
-        <img className="target-domain-profile-Logo" src="http://imgur.com/gallery/S0p8C" />
-        <ul className="target-domain-profile-ip-list">
-            <li className="target-domain-profile-ip-address">
-                
-            </li>
-        </ul>
-        <ol className="target-domain-profile-pipeline">
-            <li className="target-domain-profile-pipeline-step">Reconaissance</li>
-            <li className="target-domain-profile-pipeline-step">Scanning</li>
-            <li className="target-domain-profile-pipeline-step">Gaining Access</li>
-            <li className="target-domain-profile-pipeline-step">Maintaining Access</li>
-            <li className="target-domain-profile-pipeline-step">Cover Tracks</li>
-        </ol>
-    </div>
-);
+export default class TargetDomainProfile extends Component {
+
+    /*
+    this.props = targetDomainProfile = {
+        name: '',
+        ipAddresses: [],
+        imgURL: '',
+        
+    }
+     */
+
+    constructor (props){
+        super(props);
+        this.state = {
+            steps: {
+                reconnaissance: false,
+                scanning: false,
+                gainingAccess: false,
+                maintainingAccess: false,
+                footprinting: false
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="target-domain-profile">
+                <header className="target-domain-profile-name">
+                    Domain Name
+                </header>
+                <img className="target-domain-profile-logo" src="http://i.imgur.com/JuTHhEY.jpg"/>
+                <ol className="target-domain-profile-pipeline">
+                    <li className="target-domain-profile-pipeline-step">Reconnaissance</li>
+                    <li className="target-domain-profile-pipeline-step">Scanning</li>
+                    <li className="target-domain-profile-pipeline-step">Gaining Access</li>
+                    <li className="target-domain-profile-pipeline-step">Maintaining Access</li>
+                    <li className="target-domain-profile-pipeline-step">Footprinting</li>
+                </ol>
+                <div>
+                    <ul className="target-domain-profile-ip-list">
+                        <li className="target-domain-profile-ip-address"></li>
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
+}
